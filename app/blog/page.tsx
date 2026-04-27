d132a77789d1aea1077a5766bff8de188045de35
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-04-27 14:30:02 +0800

    implementing blog post

---
+++ app/blog/page.tsx
@@ -0,0 +1,49 @@
+import { db } from '@/lib/db';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+import { Card, CardContent } from '@/components/ui/card';
+
+export default async function BlogPage() {
+  const blogPosts = await db.blogPost.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return (
+    <main className="min-h-screen py-16 px-4">
+      <div className="max-w-3xl mx-auto">
+        <Button variant="ghost" asChild className="mb-8">
+          <Link href="/">
+            <ArrowLeft className="size-4 mr-2" />
+            Back to Home
+          </Link>
+        </Button>
+
+        <h2 className="text-2xl font-bold mb-4">Recent Post</h2>
+        {blogPosts.length > 0 ? (
+          <div className="flex flex-col space-y-4">
+            {blogPosts.map((post) => (
+              <Card
+                key={post.id}
+                className="hover:bg-accent transitions-colors"
+              >
+                <Link href={`/blog/${post.slug}`}>
+                  <CardContent className="p-4">
+                    <h3 className="font-semibold">{post.title}</h3>
+                    <p className="text-sm text-muted-foreground">
+                      {new Date(post.createdAt).toLocaleDateString()}
+                    </p>
+                  </CardContent>
+                </Link>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <div className="">
+            <p>No post foound..</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,17 @@
+export const POSTS = [
+  {
+    id: 1,
+    slug: 'learn-nextjs-basics',
+    title: 'Lear NextJS Basics',
+    content: `
+            # Learn NextJS Basics
+                Next JS is a popular React framework that helps you build fast abd SEO-friendly websites.
+            
+            ## Why Use NextJS
+                - Built-in routing system
+                - Server-side rendering and static generation
+                - Great developer experince
+                
+            **Tips:** Start with the App Router if you are building new projects.`,
+  },
+];
